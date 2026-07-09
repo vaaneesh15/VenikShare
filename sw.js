42bbf01ef0cfe5c158f9c8e6d7667c343a734026
vaaneesh15 <vaaneesh@icloud.com>
2026-07-09 18:24:21 +0700

    Update service worker cache name and fetch handling

--- sw.js
+++ sw.js
@@ -1,34 +1,19 @@
-const CACHE_NAME = 'venikshare-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.svg',
-  'https://unpkg.com/peerjs@1.5.4/dist/peerjs.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-regular-400.woff2',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-brands-400.woff2'
-];
+const CACHE_NAME = 'chatx-v3';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('fetch', event => {
+  if (event.request.destination === 'document') {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
     ))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
   );
 });
